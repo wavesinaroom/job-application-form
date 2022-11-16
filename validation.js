a2c09b26afaf09a139527a23db4f7ae488e0d100
wavesinaroom <juan@wavesinaroom.com>
2022-11-16 17:43:43 -0500

    [Task] Check for validity with checkValidity() function instead -> works better!

--- validation.js
+++ validation.js
@@ -3,8 +3,10 @@ const email = document.getElementById('e-mail');
 const notification = document.getElementById('notification');
 
 form.addEventListener('submit', (e)=>{
-  if(email.validity.valid){
-    notification.textContent = 'error'; 
+  if(!email.checkValidity())
+  {
+    alert('nah');
+    notification.textContent = 'error';
     e.preventDefault();
-  } 
+  }
 });
