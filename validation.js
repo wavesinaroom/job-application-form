d6655a694b8bc5171cb3d400375289a2ac429e1b
wavesinaroom <juan@wavesinaroom.com>
2022-11-16 17:34:02 -0500

    [Feature] Append span to notify user of errors

--- validation.js
+++ validation.js
@@ -1,8 +1,10 @@
 const form = document.querySelector('form');
 const email = document.getElementById('e-mail'); 
+const notification = document.getElementById('notification');
 
 form.addEventListener('submit', (e)=>{
-  if(!email.validity.valid){
-    alert('nah');
-    event.preventDefault();
-  } });
+  if(email.validity.valid){
+    notification.textContent = 'error'; 
+    e.preventDefault();
+  } 
+});
