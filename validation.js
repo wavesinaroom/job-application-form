9349d9f8e711465c28dd33dd53056f4d3652278b
wavesinaroom <juan@wavesinaroom.com>
2022-11-17 12:32:55 -0500

    [Task] Complete name validation implementation

--- validation.js
+++ validation.js
@@ -5,8 +5,16 @@ form.addEventListener('submit', (e)=>{
   if(!form.checkValidity()){
     e.preventDefault(); 
     notification.textContent = 'Please check your fields';
-    alert(form.name.validity.patternMismatch);
-    form.name.setCustomValidity('Wrong');
-    form.name.reportValidity();
+    checkName();
   }    
+
 });
+
+function checkName(){
+  if(form.name.validity.patternMismatch){
+    form.name.setCustomValidity('Can\'t contain numbers or special characters');
+  }else if (form.name.validity.valueMissing){
+    form.name.setCustomValidity('Required');
+  }
+  form.name.reportValidity();
+}
