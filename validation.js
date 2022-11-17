7ec87930b16974bb83f3fbe7aab2b210dff20b6f
wavesinaroom <juan@wavesinaroom.com>
2022-11-17 10:57:38 -0500

    [Task] Finally undestood how the validation API works

--- validation.js
+++ validation.js
@@ -1,10 +1,10 @@
 const form = document.querySelector('form');
-const email = document.getElementById('e-mail'); 
 const notification = document.getElementById('notification');
 
 form.addEventListener('submit', (e)=>{
   if(!form.checkValidity()){
     e.preventDefault(); 
     notification.textContent = 'Please check your fields';
+    alert(form.name.validity.patternMismatch);
   }    
 });
