824b767a05d347fa3bb7cd2f612bf66b567e2fa1
wavesinaroom <juan@wavesinaroom.com>
2022-11-17 16:23:12 -0500

    [Feature] Implement CV file validation

--- validation.js
+++ validation.js
@@ -10,6 +10,7 @@ form.addEventListener('submit', (e)=>{
     checkCity();
     checkPhoneNumber();
     checkEmail();
+    checkCVFile();
   }    
 
 });
@@ -49,6 +50,23 @@ function checkEmail(){
     notification.innerHTML += '* Invalid Email format<br/>'; 
   }else if (form.email.validity.valueMissing){
     notification.innerHTML += '* Email is required <br/>'; 
+  }
+}
+
+function checkCVFile(){
+  if(document.getElementById('cv').files[0]){
+    const fileInput = document.getElementById('cv');
+    const allowed = ['pdf', 'docx', 'txt'];
+    const extension = fileInput.files[0].name.split('.').pop();
+    const maxFileSize = 5000;
+
+    if(!allowed.includes(extension)){
+      notification.innerHTML += '* Invalid CV file type';
+    }else if(fileInput.files[0].size>maxFileSize){
+      notification.innerHTML += '* File size shouldn\'t exceed 5MB';
+    }  
+  }else{
+    notification.innerHTML += '* No CV file uploaded';
   }
 }
 
